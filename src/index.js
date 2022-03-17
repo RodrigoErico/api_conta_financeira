4b291d7a54d0c5d5bcc9c2e94a7d1dbaaa8350fe
Rodrigo Érico <devsrodrigo@gmail.com>
2022-03-17 14:22:08 -0300

    Validando conta

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ app.post("/account", (req, res) => {
         return response.status(400).json({ error: "Customer already exists!" })
     };
 
-
     curstomers.push({
         cpf,
         name,
@@ -30,4 +29,16 @@ app.post("/account", (req, res) => {
     return res.status(201).send();
 
 })
+
+app.get("/statement", (req, res) => {
+    const { cpf } = req.headers;
+
+    const customer = curstomers.find(customer => customer.cpf === cpf);
+    
+    if(!customer) {
+        return res.status(400).json({ error: "Customer not found" });
+    }
+    return res.json(customer.statement);
+});
+
 app.listen(3131);
