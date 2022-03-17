64d74a3f8c43f9b9ebf46304c55d056dda6a5fcc
Rodrigo Érico <devsrodrigo@gmail.com>
2022-03-17 16:11:46 -0300

    Criado  middleware e inserindo informações dentro do request

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const { request } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -8,6 +9,20 @@ app.use(express.json());
 
 const curstomers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = curstomers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+      return res.status(400).json({ error: "Customer not found" });
+    };
+
+    request.customer = customer;
+
+    return next();
+}
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
 
@@ -30,14 +45,8 @@ app.post("/account", (req, res) => {
 
 })
 
-app.get("/statement", (req, res) => {
-    const { cpf } = req.headers;
-
-    const customer = curstomers.find(customer => customer.cpf === cpf);
-    
-    if(!customer) {
-        return res.status(400).json({ error: "Customer not found" });
-    }
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => { 
+    const { customer } = request;
     return res.json(customer.statement);
 });
 
