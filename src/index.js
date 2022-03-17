11006345fb7a221fdcc787bdf408a0bcb5023d71
Rodrigo Érico <devsrodrigo@gmail.com>
2022-03-17 13:55:52 -0300

    Validando cpf

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -9,14 +10,22 @@ const curstomers = [];
 
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
-    const id = uuidv4();
+
+    const customerAlreadyExists = curstomers.some(
+        (customer) => customer.cpf === cpf
+    );
+    
+    if(customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" })
+    };
+
 
     curstomers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
-    })
+    });
 
     return res.status(201).send();
 
